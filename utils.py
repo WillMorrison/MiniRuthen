bfa03d39427d976d6570326d4c213e771f463d0c
Will Morrison <camocrazed+github@gmail.com>
2015-11-05 21:09:06 -0500

    first pass at summary stats accumulator

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 """Utils holds miscellaneous classes and functions that don't fit elsewhere."""
 
-import world
 import collections
+import math
+import world
 
 class YearRecord(object):
   def __init__(self):
@@ -26,3 +27,41 @@ LifetimeRecord = collections.namedtuple('LifetimeRecord',
 
 def Indexed(base, current_year, rate=1+world.PARGE):
   return base * (rate ** (current_year - world.BASE_YEAR))
+
+class SummaryStatsAccumulator(object):
+  """This uses a generalization of Welford's Algorithm by Chan et al [1] to
+  calculate mean, variance, and standard deviation in one pass, with the ability
+  to update from intermediate objects of this class as well as from single
+  data points.
+
+  [1] http://i.stanford.edu/pub/cstr/reports/cs/tr/79/773/CS-TR-79-773.pdf
+  """
+  def __init__(self):
+    self.n = 0
+    self.mean = 0
+    self.M2 = 0
+
+  def UpdateOneValue(self, value):
+    self.n += 1
+    delta = value - self.mean
+    self.mean += delta / self.n
+    self.M2 += delta * (value - self.mean)
+
+  def UpdateSubsample(self, n, mean, M2):
+    delta = mean - self.mean
+    self.mean = (self.mean * self.n + mean * n) / (self.n + n)
+    self.M2 += M2 + math.pow(delta, 2) * self.n * n / (self.n + n)
+    self.n += n
+
+  @property
+  def variance(self):
+    """Returns the sample variance, or NaN if fewer than 2 updates."""
+    if self.n > 1:
+      return self.M2 / (self.n - 1)
+    else:
+      return float('nan')
+
+  @property
+  def stddev(self):
+    """Returns the sample standard deviation, or NaN if fewer than 2 updates."""
+    return math.sqrt(self.variance)
--- utils_test.py
+++ utils_test.py
@@ -9,5 +9,57 @@ class UtilsTest(unittest.TestCase):
     self.assertAlmostEqual(utils.Indexed(100, world.BASE_YEAR, 123), 100)
     self.assertAlmostEqual(utils.Indexed(100, world.BASE_YEAR + 1), 101)
 
+  def testSummaryStatsAccumulatorUpdateOneValue(self):
+    acc = utils.SummaryStatsAccumulator()
+    for i in range(2, 52, 2):
+      acc.UpdateOneValue(i)
+
+    self.assertEqual(acc.n, 25)
+    self.assertAlmostEqual(acc.mean, 26)
+    self.assertAlmostEqual(acc.M2, 5200)
+    self.assertAlmostEqual(acc.variance, 216.666666667)
+    self.assertAlmostEqual(acc.stddev, 14.719601444)
+
+  def testSummaryStatsAccumulatorUpdateOneValueBigNumbers(self):
+    acc = utils.SummaryStatsAccumulator()
+    for i in range(1000000002, 1000000052, 2):
+      acc.UpdateOneValue(i)
+
+    self.assertEqual(acc.n, 25)
+    self.assertAlmostEqual(acc.mean, 1000000026)
+    self.assertAlmostEqual(acc.M2, 5200)
+    self.assertAlmostEqual(acc.variance, 216.666666667)
+    self.assertAlmostEqual(acc.stddev, 14.719601444)
+  
+  def testSummaryStatsAccumulatorUpdateSubsample(self):
+    acc1 = utils.SummaryStatsAccumulator()
+    acc2 = utils.SummaryStatsAccumulator()
+    for i in range(2, 26, 2):
+      acc1.UpdateOneValue(i)
+    for i in range(26, 52, 2):
+      acc2.UpdateOneValue(i)
+    acc1.UpdateSubsample(acc2.n, acc2.mean, acc2.M2)
+
+    self.assertEqual(acc1.n, 25)
+    self.assertAlmostEqual(acc1.mean, 26)
+    self.assertAlmostEqual(acc1.M2, 5200)
+    self.assertAlmostEqual(acc1.variance, 216.666666667)
+    self.assertAlmostEqual(acc1.stddev, 14.719601444)
+  
+  def testSummaryStatsAccumulatorUpdateSubsampleBigNumbers(self):
+    acc1 = utils.SummaryStatsAccumulator()
+    acc2 = utils.SummaryStatsAccumulator()
+    for i in range(1000000002, 1000000026, 2):
+      acc1.UpdateOneValue(i)
+    for i in range(10000000026, 1000000052, 2):
+      acc2.UpdateOneValue(i)
+    acc1.UpdateSubsample(acc2.n, acc2.mean, acc2.M2)
+
+    #self.assertEqual(acc1.n, 25)
+    self.assertAlmostEqual(acc1.mean, 1000000026)
+    self.assertAlmostEqual(acc1.M2, 5200)
+    self.assertAlmostEqual(acc1.variance, 216.666666667)
+    self.assertAlmostEqual(acc1.stddev, 14.719601444)
+
 if __name__ == '__main__':
   unittest.main()
