7557eabf660ccd52cfedf9426903dab239feab55
Will Morrison <camocrazed+github@gmail.com>
2015-11-08 16:18:35 -0500

    Quantile accumulator

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 """Utils holds miscellaneous classes and functions that don't fit elsewhere."""
 
+import bisect
 import collections
 import math
 import world
@@ -65,3 +66,62 @@ class SummaryStatsAccumulator(object):
   def stddev(self):
     """Returns the sample standard deviation, or NaN if fewer than 2 updates."""
     return math.sqrt(self.variance)
+
+
+class QuantileAccumulator(object):
+  """This uses a streaming parallel histogram building algorithm described by
+  Ben-Haim and Yom-Tov in [1] to accumulate values, and uses this histogram to
+  provide quantile approximations.
+
+  [1] http://jmlr.org/papers/volume11/ben-haim10a/ben-haim10a.pdf
+  """
+  def __init__(self, max_bins=100):
+    self.max_bins = max_bins
+    self.bins = []
+
+  def _Merge(self):
+    """Merges bins if there are more than max_bins. Expects self.bins to be sorted"""
+    while len(self.bins) > self.max_bins:
+      # Find the two closest bins
+      sep, i = min((self.bins[i+1][0] - self.bins[i][0], i) for i in range(len(self.bins)-1))
+
+      # Merge them
+      self.bins[i:i+2] = [(
+          (self.bins[i][0]*self.bins[i][1] + self.bins[i+1][0]*self.bins[i+1][1])/(self.bins[i][1]+self.bins[i+1][1]),
+          self.bins[i][1]+self.bins[i+1][1])]
+
+  def UpdateOneValue(self, value):
+    bisect.insort(self.bins, (value, 1))
+    self._Merge()
+
+  def UpdateHistogram(self, bins):
+    self.bins.extend(bins)
+    self.bins.sort()
+    self._Merge()
+
+  def Quantile(self, q):
+    if q < 0 or 1 < q:
+      raise ValueError("quantile should be a number between 0 and 1, inclusive")
+
+    # Cumulative sum of the counts at each bin point, treating the point as the center of the bin
+    bin_counts = [0] + [b[1] for b in self.bins] + [0]
+    cumsums = [0]
+    for i in range(1, len(bin_counts)):
+      bin_count = (bin_counts[i] + bin_counts[i-1])/2
+      cumsums.append(cumsums[-1] + bin_count)
+
+    # Find the index of the interval in which the desired quantile lies
+    n_points = q * cumsums[-1]
+    i = bisect.bisect(cumsums, n_points)-1
+
+    if i <= 0:
+      # special case, quantile falls before first bin
+      return self.bins[0][0]
+    elif i >= len(self.bins):
+      # Special case, quantile falls at or after last bin
+      return self.bins[-1][0]
+    else:
+      bin_frac = (n_points - cumsums[i])/(cumsums[i+1] - cumsums[i])
+      return self.bins[i-1][0] + bin_frac * (self.bins[i][0] - self.bins[i-1][0])
+
+    
--- utils_test.py
+++ utils_test.py
@@ -61,5 +61,92 @@ class UtilsTest(unittest.TestCase):
     self.assertAlmostEqual(acc1.variance, 216.666666667)
     self.assertAlmostEqual(acc1.stddev, 14.719601444)
 
+  def assertHistogramsEqual(self, hist1, hist2, places=7):
+    """Compares two lists of (float, int) tuples for equality."""
+    fail = False
+    if len(hist1) != len(hist2):
+      fail = True
+    else:
+      for a, b in zip(hist1, hist2):
+        if round(a[0]-b[0], places) != 0 or a[1] != b[1]:
+          fail = True
+    if fail:
+      self.fail("Histograms differ: a=%r, b=%r" % (hist1, hist2))
+
+  def testQuantileAccumulatorUpdateOneValueNoMerge(self):
+    acc = utils.QuantileAccumulator(max_bins=3)
+    acc.UpdateOneValue(5)
+    acc.UpdateOneValue(22)
+    acc.UpdateOneValue(9)
+
+    self.assertHistogramsEqual(acc.bins, [(5, 1), (9, 1), (22, 1)])
+
+
+  def testQuantileAccumulatorUpdateOneValueMerge(self):
+    acc = utils.QuantileAccumulator(max_bins=2)
+    acc.UpdateOneValue(5)
+    acc.UpdateOneValue(22)
+    acc.UpdateOneValue(9)
+
+    self.assertHistogramsEqual(acc.bins, [(7, 2), (22, 1)])
+
+  def testQuantileAccumulatorUpdateHistogramNoMerge(self):
+    acc1 = utils.QuantileAccumulator(max_bins=4)
+    acc1.UpdateOneValue(5)
+    acc1.UpdateOneValue(22)
+    acc2 = utils.QuantileAccumulator(max_bins=2)
+    acc2.UpdateOneValue(9)
+    acc2.UpdateOneValue(19)
+
+    acc1.UpdateHistogram(acc2.bins)
+
+    self.assertHistogramsEqual(acc1.bins, [(5, 1), (9, 1), (19, 1), (22, 1)])
+
+  def testQuantileAccumulatorUpdateHistogramMerge(self):
+    acc1 = utils.QuantileAccumulator(max_bins=2)
+    acc1.UpdateOneValue(5)
+    acc1.UpdateOneValue(22)
+    acc2 = utils.QuantileAccumulator(max_bins=2)
+    acc2.UpdateOneValue(9)
+    acc2.UpdateOneValue(19)
+
+    acc1.UpdateHistogram(acc2.bins)
+
+    self.assertHistogramsEqual(acc1.bins, [(7, 2), (20.5, 2)])
+
+  def testQuantileAccumulatorUpdateHistogramPaperExample(self):
+    acc = utils.QuantileAccumulator(max_bins=5)
+    acc.bins = [(2, 1), (9.5, 2), (17.5, 2), (23, 1), (36,1)]
+    acc.UpdateHistogram([(32, 1), (30, 1), (45, 1)])
+
+    self.assertHistogramsEqual(acc.bins, [(2, 1), (9.5, 2), (19.3333333, 3), (32.6666667, 3), (45, 1)])
+
+  def testQuantileAccumulatorQuantileLots(self):
+    acc = utils.QuantileAccumulator(max_bins=100)
+    for i in range(10001):
+      acc.UpdateOneValue(i/10)
+
+    self.assertAlmostEqual(acc.Quantile(0.5), 500, places=0)
+    self.assertAlmostEqual(acc.Quantile(0.2), 200, places=0)
+    self.assertAlmostEqual(acc.Quantile(0.1), 100, places=0)
+
+  def testQuantileAccumulatorQuantileBeforeAfter(self):
+    acc = utils.QuantileAccumulator()
+    acc.bins = [(1, 10), (2, 5), (3, 10)]
+
+    self.assertAlmostEqual(acc.Quantile(0), 1)
+    self.assertAlmostEqual(acc.Quantile(0.1), 1)
+    self.assertAlmostEqual(acc.Quantile(0.9), 3)
+    self.assertAlmostEqual(acc.Quantile(1), 3)
+
+  def testQuantileAccumulatorQuantileExact(self):
+    acc = utils.QuantileAccumulator()
+    acc.bins = [(1, 10), (2, 5), (3, 10)]
+
+    self.assertAlmostEqual(acc.Quantile(0.4), 1.6666667)
+    self.assertAlmostEqual(acc.Quantile(0.5), 2)
+    self.assertAlmostEqual(acc.Quantile(0.6), 2.3333333)
+
+
 if __name__ == '__main__':
   unittest.main()
